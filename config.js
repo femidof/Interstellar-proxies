d2d0019b48bb42b9627182aad17f6931a9b39fec
Cold Code <dee_femifose@yahoo.com>
2024-04-10 18:35:48 -0500

    adding passwords and users

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+    // interstellar: 'password',
+    converky:"9ZiCroq1YW80853TFcVT",
+    abankamy:"13mopPW1kGqqLkc95KEd",
+    cartione:"fYiw361X16FkeYfxqoDD",
+    hedist: "2z9evYLW9Wd65H1Gcjde",
+    equayous: "y8nBUuNQaTs41bjFIej3",
   },
   routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
+  local: false, // Change this to false to disable local assets.
 }
 export default config
