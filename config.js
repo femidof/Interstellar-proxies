5f81569c1be34ca81bb349efb72d186177e5e73e
Cold Code <dee_femifose@yahoo.com>
2024-04-10 18:40:05 -0500

    Revert "adding passwords and users"
    This reverts commit d2d0019b48bb42b9627182aad17f6931a9b39fec.

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
 const config = {
-  challenge: true, // Set to true if you want to enable password protection.
+  challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    // interstellar: 'password',
-    converky:"9ZiCroq1YW80853TFcVT",
-    abankamy:"13mopPW1kGqqLkc95KEd",
-    cartione:"fYiw361X16FkeYfxqoDD",
-    hedist: "2z9evYLW9Wd65H1Gcjde",
-    equayous: "y8nBUuNQaTs41bjFIej3",
+    interstellar: 'password',
   },
   routes: true, // Change this to false if you just want to host a bare server.
-  local: false, // Change this to false to disable local assets.
+  local: true, // Change this to false to disable local assets.
 }
 export default config
